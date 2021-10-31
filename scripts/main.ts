d0b7a0a8640440829521cc80735a11bc4fdf3e57
BuggedPotato <donko812@gmail.com>
2021-10-31 22:23:04 +0100

    Fix type errors in main.ts and compile for prod

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,6 +1,7 @@
 import { Fridge } from "./classes/Fridge";
 import { DBHandle } from "./classes/DBHandle";
 import { StickyNote } from "./classes/Note";
+import { XYBase } from "./interfaces/intXYBase";
 
 const urlParams = new URLSearchParams(window.location.search);
 
@@ -15,20 +16,21 @@ getFridge( fridge, a);
 
 async function getFridge( inFridge : Fridge, name : string )
 {
-    let tmp : Object = await DBHandle.getFridgeData( name );
+    let tmp : helpQ = await DBHandle.getFridgeData( name ) as helpQ;
     if( !tmp )
     {
         inFridge.functions4Notes.saveMe();
         return;
     }
 
-    let f : Object = JSON.parse( tmp.Data );
+    let f : helpF = JSON.parse( tmp.Data );
+    console.log( f );
     let arr : Array<Object> = JSON.parse( tmp.StickyNotesTab );
 
     inFridge.totalNotes = f.totalNotes;
     inFridge.currentNotes = f.currentNotes;
     inFridge.setTop( f.topIndex );
-    inFridge.stickyNotes = arr.map( (el)=>{
+    inFridge.stickyNotes = arr.map( ( el : helpS )=>{
         return new StickyNote( el.id, inFridge.functions4Notes, el.size, el.position, el.text, el.zIndex );
     } );
 
@@ -48,4 +50,25 @@ document.body.onload = ()=>{
     // oh and adds some listeners I guess
 };
 
-
+interface helpQ{
+    Data : string;
+    ID : string;
+    Nazwa : string;
+    StickyNotesTab : string;
+}
+interface helpF{
+    currentNotes : number;
+    functions4Notes : Object;
+    name : string;
+    stickyNotes : Array<Object>;
+    topIndex : number;
+    totalNotes : number;
+}
+interface helpS{
+    id : number;
+    parentFunctions : Object;
+    position : XYBase;
+    size : XYBase;
+    text : string;
+    zIndex : number;
+}
